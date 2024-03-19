8c23b728e22f603afc19698d1346d715c8cbedc3
Brayanjax <120285750+Brayanjax@users.noreply.github.com>
2024-03-18 20:48:31 -0600

    Funcionamiento Login y estilo

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Swal from 'sweetalert2';
-
+import logo from './img/pexels-eberhard-grossgasteiger-2310641.jpg'; 
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -20,21 +20,30 @@ const LoginPage: React.FC = () => {
         }
       });
       console.log(response.data);
-      Swal.fire({
-        title: 'Bienvenido!',
-        text: response.data.name,
-        icon: 'success',
-        confirmButtonText: 'Ingresar'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          navigate('/calendar');
-        }
-      });
+      if (response.data.email === email && response.data.password === password) {
+        Swal.fire({
+          title: 'Bienvenido!',
+          text: response.data.name,
+          icon: 'success',
+          confirmButtonText: 'Ingresar'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            navigate('/calendar');
+          }
+        });
+      } else {
+        Swal.fire({
+          title: 'Error!',
+          text: 'Usuario o contraseña incorrectos',
+          icon: 'error',
+          confirmButtonText: 'Intentar de nuevo'
+        });
+      }
     } catch (error) {
       console.error(error);
       Swal.fire({
         title: 'Error!',
-        text: 'Usuario o contraseña incorrectos',
+        text: 'Ocurrió un error al iniciar sesión',
         icon: 'error',
         confirmButtonText: 'Intentar de nuevo'
       });
@@ -42,42 +51,59 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-<div className="login-form-container shadow p-3 mb-5 mt-5  bg-white rounded">
-  <form onSubmit={handleLogin}>
-    <div className="mb-3">
-      <label htmlFor="email" className="form-label">
-        Email:
-      </label>
-      <input
-        type="email"
-        className="form-control"
-        id="email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
-    </div>
-    <div className="mb-3">
-      <label htmlFor="password" className="form-label">
-        Password:
-      </label>
-      <input
-        type="password"
-        className="form-control"
-        id="password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-    </div>
-    <div className="d-grid gap-2">
-      <button type="submit" className="btn btn-primary">
-        Login
-      </button>
-    </div>
-  </form>
-</div>
+    <body style={{ background :"#333", color:"#fff" }}>
+      <div className="container">
+      <div className="row">
+        <div className="col-md-6" id="ladoIzquierdo">
+          <div className="card" style={{ width: "28rem",background :"#333", border:"none", color:"#fff" }}>
+            <div className="card-body">
+              <h5 className="card-title my-5">Iniciar Sesión</h5>
+              <form onSubmit={handleLogin}>
+                <div className="mb-4 py-2">
+                  <label htmlFor="email" className="form-label">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    aria-describedby="emailHelp"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="password" className="form-label">
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="passwordInput"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
+                <button type="button" className="btn btn-secondary" id="registroBtn">
+                  Registrarse
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
 
-
+        <div className="col-md-6" id="imagenIzquierda">
+          <img src={logo} alt="Descripción de la imagen" className="img-fluid mt-3" />
+        </div>
+      </div>
+    </div>
+    </body>
+    
   );
 };
 
 export default LoginPage;
+
